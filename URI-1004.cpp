c5f6b564c8e393c29bb22d73552d11cc4fa083e7
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-01-26 08:36:44 -0300

    URI 1004

---
+++ URI-1004.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num1, num2, PROD;
+
+    cin >> num1;
+    cin >> num2;
+
+    PROD = num1 * num2;
+
+    cout << "PROD = "<< PROD << "\n";
+
+
+
+    return 0;
+}
