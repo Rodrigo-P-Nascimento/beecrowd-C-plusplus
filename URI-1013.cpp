6247124be3dff334c8b4cd78503788e1bdd7463b
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-02-08 10:26:04 -0300

    Create URI-1013.cpp

---
+++ URI-1013.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a, b, c;
+    int maior;
+
+    cin >> a >> b >> c;
+
+    maior = (a+b+abs(a-b))/2;
+
+    maior = (maior+c+abs(maior-c))/2;
+
+    cout << maior << " eh o maior" << endl;
+
+    return 0;
+}
