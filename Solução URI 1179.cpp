6e6fb5d21dba1b507d8523d93731570e8900e7e1
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-02-11 10:47:38 -0300

    up

--- Solução URI 1179.cpp
+++ Solução URI 1179.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
@@ -6,13 +7,46 @@ int main(){
 
     int vetor_par[5];
     int vetor_impar[5];
-    int num;
+    int num, cont_par=0, cont_impar=0;
+    int aux1=0, aux2=0;
 
     for(int i=0; i < 15; i++){
         cin >> num;
+
+        if(num%2 == 0){
+            if(cont_par < 5){
+                vetor_par[cont_par] = num;
+                cont_par++;
+                aux1++;
+            }
+            if(cont_par == 5){
+                cont_par = 0;
+                for(int j=0; j < 5;j++){
+                    printf("par[%d] = %d\n", j, vetor_par[j]);
+                }
+
+            }
+        }else{
+            if(cont_impar < 5){
+                vetor_impar[cont_impar] = num;
+                cont_impar++;
+                aux2++;
+            }
+            if(cont_impar == 5){
+                cont_impar = 0;
+                for(int j=0; j < 5;j++){
+                    printf("impar[%d] = %d\n", j, vetor_impar[j]);
+                }
+            }
+        }
     }
 
-
+    for(int i=0; i < (aux2-5);i++){
+         printf("impar[%d] = %d\n", i, vetor_impar[i]);
+    }
+    for(int i=0; i < (aux1-5);i++){
+         printf("par[%d] = %d\n", i, vetor_par[i]);
+    }
 
     return 0;
 }
