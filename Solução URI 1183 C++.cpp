f65d861cb8584c2a45e09f24d1bbf64c5750471d
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-02-12 09:06:13 -0300

    Update Solução URI 1183 C++.cpp

--- Solução URI 1183 C++.cpp
+++ Solução URI 1183 C++.cpp
@@ -7,10 +7,13 @@ int main(){
 
     double matriz[12][12];
     int i, j;
-    double soma;
+    double soma=0;
     char op;
 
-    scanf("%*c", &op);
+    cout.precision(1);
+    cout << fixed;
+
+    scanf("%c", &op);
 
     for(i=0;i<12;i++){
         for(j=0;j<12;j++){
@@ -20,10 +23,16 @@ int main(){
 
     for(i=0;i<12;i++){
         for(j=i+1;j<12;j++){
-            soma+=matri[i][j];
+            soma+=matriz[i][j];
         }
     }
-
+    if(op == 'S'){
+        cout << soma << endl;
+    }
+    if(op == 'M'){
+        soma = soma / 66.0;
+        cout << soma << endl;
+    }
 
     return 0;
 }
